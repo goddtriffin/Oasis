8f42c22a5aac32e269eeb7802e623eeac2115d74
Todd Griffin <nemagnusfrater@gmail.com>
2018-04-20 03:25:52 -0400

    Add client handler to socket 'disconnect' event to transfer the user to the serverdown page.

--- public/scripts/initSocket.js
+++ public/scripts/initSocket.js
@@ -2,3 +2,8 @@ const debug = false;
 
 // intitialize socket.io client socket
 const socket = io();
+
+// handle when the server goes down
+socket.on('disconnect', function () {
+    location.href = "../pages/serverdown/";
+});
