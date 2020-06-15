6bdfd26f22a85cef6f57b4d834935362ec410cca
Todd Everett Griffin <goddtriffin@gmail.com>
2020-06-15 02:57:41 -0500

    Update all dependencies. Refactor setup and start scripts to better handle making directories that already exist. Fix startup message to correctly display which port the server is listening on.

--- server/app.js
+++ server/app.js
@@ -1,24 +1,25 @@
 // express / socket.io
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 // clients
-const clients = require('./clients');
+const clients = require("./clients");
 
 // allow access to everything in these folders
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // home requested
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
 // handle the connection of clients
 clients.init(io);
 
 // start server
-http.listen(process.env.PORT || 80, function () {
-    console.log('Server listening on *:8633!');
+var port = process.env.PORT || 80;
+http.listen(port, function () {
+  console.log("Server listening on *:" + port + "!");
 });
